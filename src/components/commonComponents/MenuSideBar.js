aa3ca897d97a72c7b4b308f50055df8d24f745ae
Horingring <dldldksl@naver.com>
2020-12-09 03:59:52 +0900

    1209_myPortfolio_Make MyInfo Component2_MenuSideBar fixed 클래스 부여하기
    - scrollBar가 일정한 포인트를 기점으로 <MenuSideBar>의 position이 fixed로 변경되도록 구현하기 성공!!!!! 야호!!!!!
    
    구현방법
    1) window.eventListener('scroll', onScroll); 을 통해, 스크롤 시 onScroll이 실행되도록 했다.
    2) onScroll 내부의 if문을 통해, state가 변경되도록 하였고, 이 state에 따라 <MenuSideBar>의 클래스가 결정되도록 하였다. 
      ( * 템플릿 리터럴(Template Literal) 문법 사용)

--- src/components/commonComponents/MenuSideBar.js
+++ src/components/commonComponents/MenuSideBar.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MenuSideBarTab from "./MenuSideBarTab";
 
 const MenuSideBar = () => {
+  const [onFixed, setOnFixed] = useState(false);
+  const onScroll = (e) => {
+    const scrollTop = ("scroll", e.srcElement.scrollingElement.scrollTop);
+    // console.log(scrollTop);
+    if (scrollTop >= 722.4) {
+      setOnFixed(true);
+    } else {
+      setOnFixed(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
   return (
-    <div className="menuSideBar-wrapper">
+    <div className={`menuSideBar-wrapper${onFixed ? " onFixed" : ""}`}>
       <div className="menuSideBar">
         <MenuSideBarTab iconName="faHome" />
         <MenuSideBarTab iconName="faUser" />
