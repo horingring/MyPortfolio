fe61a5f6733df97bfc238fc33094e84a9b5f1a54
Horingring <dldldksl@naver.com>
2020-12-16 21:32:50 +0900

    1216_myPortfolio_Project_PageContentSection_make project Article layout
    작업내용
    - <Project> - <PageContentSection> - 하위의 project article의 layout을 만듬.
    - hover시 해당 project의 주제가 fadeIn으로 나타나도록 함.
    
    추후 작업 예정
    - 각 project article 클릭 시 해당 프로젝트를 상세히 볼 수 있는 모달창 띄우기/없애기

--- src/components/commonComponents/PageContentSection.js
+++ src/components/commonComponents/PageContentSection.js
@@ -2,14 +2,58 @@ import React from "react";
 
 const PageContentSection = (props) => {
   const { contentTitle } = props;
+
+  const getSectionArticle = () => {
+    if (contentTitle === "Strength") {
+      return (
+        <>
+          <div className="sectionArticle-column">1</div>
+          <div className="sectionArticle-column">2</div>
+        </>
+      );
+    } else if (contentTitle === "My Info") {
+      return (
+        <>
+          <div className="sectionArticle-column">1</div>
+          <div className="sectionArticle-column">2</div>
+        </>
+      );
+    } else if (contentTitle === "Web Tech") {
+      return (
+        <>
+          <div className="sectionArticle-column">1</div>
+          <div className="sectionArticle-column">2</div>
+        </>
+      );
+    } else if (contentTitle === "Project") {
+      return (
+        <>
+          <div className="sectionArticle-column project-article">
+            <div className="project-article-image">
+              <div className="project-article-overlay">
+                <p>Pizza Order</p>
+                <p>Service</p>
+              </div>
+            </div>
+          </div>
+          {/*  */}
+          <div className="sectionArticle-column project-article">
+            <div className="project-article-image">
+              <div className="project-article-overlay">
+                <p>Web Portfolio</p>
+              </div>
+            </div>
+          </div>
+        </>
+      );
+    }
+  };
+
   return (
     <div className="pageContentSection-wrapper">
       <div className="pageContentSection">
         <h2>{contentTitle}</h2>
-        <div className="sectionArticle flexRow">
-          <div className="sectionArticle-column">1</div>
-          <div className="sectionArticle-column">2</div>
-        </div>
+        <div className="sectionArticle flexRow">{getSectionArticle()}</div>
       </div>
     </div>
   );
