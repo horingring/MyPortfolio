2f447b878af22b117ed40ec405e6acb97f598866
Horingring <dldldksl@naver.com>
2020-12-31 17:02:03 +0900

    1231_myPortfolio_Complete WebTech Content of Project Component
    작업내용
    - <Project> 컴포넌트의 Web Tech 내용 채움(그래프)
    - <Project> 컴포넌트의 .sectionArticle-column의 레이아웃 일부 변경(padding)
    
    추후 작업 내용
    - PageContentSection 컴포넌트의 getSectionArticle 함수의 내부의 장황한 JSX를 깔끔하게 정기(새 컴포넌트 만들기)
    - <Project>의 모달창 내용 채우기

--- src/components/commonComponents/PageContentSection.js
+++ src/components/commonComponents/PageContentSection.js
@@ -5,6 +5,12 @@ import copy from "../../img/logo/pursuit_archives.png";
 import link from "../../img/logo/pursuit_chat.png";
 import bulb from "../../img/logo/pursuit_entrepreneur.png";
 import myPicture from "../../img/myInfo_myPicture.jpg";
+import htmlLogo from "../../img/logo/html_logo.png";
+import cssLogo from "../../img/logo/css_logo.png";
+import jsLogo from "../../img/logo/js_logo.png";
+import reactLogo from "../../img/logo/React_logo.png";
+import githubLogo from "../../img/logo/github_logo.png";
+import sassLogo from "../../img/logo/sass_logo.png";
 
 const PageContentSection = (props) => {
   const { contentTitle } = props;
@@ -131,8 +137,88 @@ const PageContentSection = (props) => {
     } else if (contentTitle === "Web Tech") {
       return (
         <>
-          <div className="sectionArticle-column">1</div>
-          <div className="sectionArticle-column">2</div>
+          <div className="sectionArticle-column">
+            <div className="webTech-box-wrapper">
+              <div className="webTech-box">
+                <div className="webTechImage-wrapper">
+                  <img src={htmlLogo} alt="html_logo"></img>
+                </div>
+                <div className="webTechGraph-wrapper">
+                  <div className="webTechGraph webTechGraph_html">
+                    <span>HTML</span>
+                  </div>
+                  <div className="webTechGraphPercent">80%</div>
+                </div>
+              </div>
+            </div>
+            <div className="webTech-box-wrapper">
+              <div className="webTech-box">
+                <div className="webTechImage-wrapper">
+                  <img src={cssLogo} alt="css_logo"></img>
+                </div>
+                <div className="webTechGraph-wrapper">
+                  <div className="webTechGraph webTechGraph_css">
+                    <span>CSS</span>
+                  </div>
+                  <div className="webTechGraphPercent">80%</div>
+                </div>
+              </div>
+            </div>
+            <div className="webTech-box-wrapper">
+              <div className="webTech-box">
+                <div className="webTechImage-wrapper">
+                  <img src={jsLogo} alt="js_logo"></img>
+                </div>
+                <div className="webTechGraph-wrapper">
+                  <div className="webTechGraph webTechGraph_js">
+                    <span>Javascript</span>
+                  </div>
+                  <div className="webTechGraphPercent">60%</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="sectionArticle-column">
+            <div className="webTech-box-wrapper">
+              <div className="webTech-box">
+                <div className="webTechImage-wrapper">
+                  <img src={reactLogo} alt="react_logo"></img>
+                </div>
+                <div className="webTechGraph-wrapper">
+                  <div className="webTechGraph webTechGraph_react">
+                    <span>React</span>
+                  </div>
+                  <div className="webTechGraphPercent">70%</div>
+                </div>
+              </div>
+            </div>
+            <div className="webTech-box-wrapper">
+              <div className="webTech-box">
+                <div className="webTechImage-wrapper">
+                  <img src={githubLogo} alt="github_logo"></img>
+                </div>
+                <div className="webTechGraph-wrapper">
+                  <div className="webTechGraph webTechGraph_github">
+                    <span>Github</span>
+                  </div>
+                  <div className="webTechGraphPercent">40%</div>
+                </div>
+              </div>
+            </div>
+            <div className="webTech-box-wrapper">
+              <div className="webTech-box">
+                <div className="webTechImage-wrapper">
+                  <img src={sassLogo} alt="sass_logo"></img>
+                </div>
+                <div className="webTechGraph-wrapper">
+                  <div className="webTechGraph webTechGraph_sass">
+                    <span>Sass</span>
+                  </div>
+                  <div className="webTechGraphPercent">50%</div>
+                </div>
+              </div>
+            </div>
+          </div>
         </>
       );
     } else if (contentTitle === "Project") {
